e95b271aae2bc3011f427076a211033a988dbff7
angburmun <angburmun@gmail.com>
2025-01-02 20:49:37 +0100

    Contenedor básico funcionando

--- app.py
+++ app.py
@@ -58,4 +58,4 @@ def predict():
 
 if __name__ == '__main__':
     app.logger.info("Iniciando la aplicación Flask en modo debug.")
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
